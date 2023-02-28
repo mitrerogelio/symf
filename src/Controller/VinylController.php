ddb2b451e442c73473607e370a237ecffda03898
Rogelio Mitre <mitrerogelio@outlook.com>
2023-02-28 15:20:11 -0600

    add twig templating

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PH and Jams');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'tracks' => $tracks,
+            'title' => 'PB & Jams'
+        ]);
     }
 
     #[Route('/browse/{genre}')]
